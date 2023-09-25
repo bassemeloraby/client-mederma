378a59a8c57d7a25e444169a0ece42af3c189d92
bassem <basem12683@gmail.com>
2023-09-25 03:51:52 +0300

    done

--- src/pages/medicine/Indication.js
+++ src/pages/medicine/Indication.js
@@ -2,17 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../../components/Spinner";
-// import Table from "react-bootstrap/Table";
+import Table from "react-bootstrap/Table";
 // import { mainUrl } from "../../data";
 // const url = mainUrl + "indications";
 
 function Indication() {
-    const { ScientificName } = useParams();
-    // eslint-disable-next-line
-    const [allDrugs, setAllDrugs] = useState([]);
-    const [loading, setLoading] = useState(false);
+  const { ScientificName } = useParams();
+  // eslint-disable-next-line
+  const [allDrugs, setAllDrugs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
 
- useEffect(() => {
+  useEffect(() => {
     const fetchAllDrugs = async () => {
       setLoading(true);
       try {
@@ -28,16 +29,42 @@ function Indication() {
     fetchAllDrugs();
   }, []);
 
+  useEffect(() => {
+    const afilter = allDrugs.filter(
+      (drug) => drug.SCIENTIFIC_NAME === ScientificName
+    );
+    setItems(afilter);
+    console.log(afilter)
+  }, [allDrugs, ScientificName]);
+
   if (loading) {
     return <Spinner />;
   }
 
   return (
     <div>
-    Indication
-    {ScientificName}
+      Indication
+      {ScientificName}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>INDICATION</th>
+            <th>ICD_10_CODE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((drug, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{drug.INDICATION}</td>
+              <td> {drug.ICD_10_CODE}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
 
-export default Indication
+export default Indication;
