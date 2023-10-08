ca9f8d22ea67f93d364367efc91797bb71f3b643
bassem <basem12683@gmail.com>
2023-10-08 13:08:34 +0300

    ins updates

--- src/pages/insurance/Insurance.js
+++ src/pages/insurance/Insurance.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import insc from "../../images/inscTel.png";
+import insc1 from "../../images/inscTel-1.png";
+import insc2 from "../../images/inscTel-2.png";
+import insc3 from "../../images/inscTel-3.png";
+import insc4 from "../../images/inscTel-4.png";
+import insc5 from "../../images/inscTel-5.png";
 import { insuranceLinks } from "../../data";
 function Insurance() {
   return (
     <section className="">
-      <Nav  className="bg-dark mb-2">
+      <Nav className="bg-dark mb-2">
         {insuranceLinks.map((link, index) => (
           <Nav.Item key={index}>
             <Nav.Link href={link.link} target="_blank">
@@ -22,9 +26,13 @@ function Insurance() {
         ></iframe>
       </div>
 
-      <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-around flex-wrap">
         {" "}
-        <img src={insc} alt="insurance"></img>
+        <img src={insc1} alt="insurance1" className=""></img>
+        <img src={insc2} alt="insurance2"></img>
+        <img src={insc3} alt="insurance3"></img>
+        <img src={insc4} alt="insurance4"></img>
+        <img src={insc5} alt="insurance5"></img>
       </div>
     </section>
   );
