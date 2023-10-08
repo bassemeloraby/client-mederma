580cfd57985a78aaad20976baf5a8c04972201f1
bassem <basem12683@gmail.com>
2023-10-08 12:43:31 +0300

    ins updates

--- src/pages/insurance/Insurance.js
+++ src/pages/insurance/Insurance.js
@@ -1,29 +1,31 @@
 import React from "react";
-import YouTube from "react-youtube";
-import Nav from 'react-bootstrap/Nav';
-// import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
 import insc from "../../images/inscTel.png";
 import { insuranceLinks } from "../../data";
-// import Zoom from "react-zoom-image-hover";
 function Insurance() {
   return (
-    <section className="insurance">
-    <Nav
-  >
-{insuranceLinks.map((link, index) =>  <Nav.Item>
-    <Nav.Link href={link.link}  target="_blank">{link.text}</Nav.Link>
-  </Nav.Item>)}
-  </Nav>
-      <section className="home-video">
-        <YouTube videoId="7wX3Pnjecqc" />
-      </section>
+    <section className="">
+      <Nav  className="bg-dark mb-2">
+        {insuranceLinks.map((link, index) => (
+          <Nav.Item key={index}>
+            <Nav.Link href={link.link} target="_blank">
+              {link.text}
+            </Nav.Link>
+          </Nav.Item>
+        ))}
+      </Nav>
+      <div className="ratio ratio-16x9">
+        <iframe
+          src="https://www.youtube.com/embed/7wX3Pnjecqc"
+          title="GlobeMed i care جلوبميد"
+          allowFullScreen
+        ></iframe>
+      </div>
+
       <div className="d-flex justify-content-center">
         {" "}
         <img src={insc} alt="insurance"></img>
       </div>
-      {/*<div>
-        <Zoom height={500} width={830} zoomScale={3} src={insc} />
-  </div>*/}
     </section>
   );
 }
