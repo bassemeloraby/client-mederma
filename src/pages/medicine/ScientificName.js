363a263cf25f611f177582f8f1c4e92ae199664f
bassem <basem12683@gmail.com>
2023-10-08 14:56:50 +0300

    g-p

--- src/pages/medicine/ScientificName.js
+++ src/pages/medicine/ScientificName.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams ,Link} from "react-router-dom";
 import axios from "axios";
 import Spinner from "../../components/Spinner";
 import Table from "react-bootstrap/Table";
 import { mainUrl } from "../../data";
 const url = mainUrl + "allDrugs";
+const l1 = "https://www.google.com/search?q=";
+const l2 =
+  "&sca_esv=571711580&rlz=1C1VDKB_enSA1075SA1075&tbm=isch&sxsrf=AM9HkKkhm2K1JYgiDZSvrn-lnYR52Xi5vA:1696763801819&source=lnms&sa=X&ved=2ahUKEwj18LLdqeaBAxXD2wIHHfNMAzMQ_AUoAXoECAQQAw&biw=1366&bih=641&dpr=1";
 
 function ScientificName() {
   const { ScientificName } = useParams();
@@ -51,7 +54,18 @@ function ScientificName() {
             .map((drug, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>{drug.TradeName} {drug.Strength} {drug.StrengthUnit}</td>
+                <td>{drug.TradeName} {drug.Strength} {drug.StrengthUnit} &nbsp;
+                <Link
+                to={
+                  l1 + drug.TradeName + drug.Strength + drug.StrengthUnit + l2
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ color: "red" }}
+              >
+                Google Pic
+              </Link>
+                </td>
                 <td> {drug.PharmaceuticalForm}</td>
                 <td> {drug.PublicPrice} SR</td>
               </tr>
