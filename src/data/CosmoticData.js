87fad2c5943b78b5b011e05ac14cce8b8b2a5b58
bassem <basem12683@gmail.com>
2023-10-28 11:04:33 +0300

    skin kind

--- src/data/CosmoticData.js
+++ src/data/CosmoticData.js
@@ -200,3 +200,50 @@ export const updateFormDb = [
     ],
   },
 ];
+
+export const skinKindDb = [
+  {
+    id: 1,
+    text: "skinSenstivety",
+  },
+  {
+    id: 2,
+    text: "normalSkin",
+  },
+  {
+    id: 3,
+    text: "drySkin",
+  },
+  {
+    id: 4,
+    text: "oilySkin",
+  },
+  {
+    id: 5,
+    text: "combinationSkin",
+  },
+  {
+    id: 6,
+    text: "atopicSkin",
+  },
+  {
+    id: 7,
+    text: "akne-poreSkin",
+  },
+  {
+    id: 8,
+    text: "hyperPigmentedSkin",
+  },
+  {
+    id: 9,
+    text: "dehydratedSkin",
+  },
+  {
+    id: 10,
+    text: "iritatedSkin",
+  },
+  {
+    id: 11,
+    text: "damagedSkin",
+  },
+];
