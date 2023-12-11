5dc95633b6547aa62a513e4069d074c18e1b3e2a
bassem <basem12683@gmail.com>
2023-12-11 11:01:45 +0300

    tot css

--- src/components/medicine/Tot.jsx
+++ src/components/medicine/Tot.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
 
 const Tot = () => {
   const [master, setMaster] = useState(0);
@@ -19,21 +20,104 @@ const Tot = () => {
   };
 
   return (
-    <div className="mt-2 row col-2">
-      <input value={master} onChange={(e) => setMaster(e.target.value)} />
-      <input value={span} onChange={(e) => setSpan(e.target.value)} />
-      <input value={visa} onChange={(e) => setVisa(e.target.value)} />
-      <hr />
-      <span className="border bg-warning">bank = {bank}</span>
-      <span>cash = {tot - (parseInt(bank) + parseInt(insurance))} </span>
-      insurance
-      <input value={insurance} onChange={(e) => setInsurance(e.target.value)} />
-      tot
-      <input value={tot} onChange={(e) => setTot(e.target.value)} />
-      vat
-      <input value={vat} onChange={(e) => setVat(e.target.value)} />
-      <span>tot-vat = {tot - vat} </span>
-      <button onClick={clear} className="bg-danger">clear</button>
+    <div className="mt-2 d-flex">
+      <section className="1 col-3 me-2">
+        {" "}
+        <Table striped bordered hover variant="primary">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Master</td>
+              <td>
+                {" "}
+                <input
+                  value={master}
+                  onChange={(e) => setMaster(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Span</td>
+              <td>
+                {" "}
+                <input value={span} onChange={(e) => setSpan(e.target.value)} />
+              </td>
+            </tr>
+            <tr>
+              <td>Visa</td>
+              <td>
+                <input value={visa} onChange={(e) => setVisa(e.target.value)} />
+              </td>
+            </tr>
+            <tr>
+              <td>Bank</td>
+              <td className="text-center">
+                <h3 className="border bg-warning"> {bank}</h3>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </section>
+      <section className="2 col-3">
+        {" "}
+        <Table striped bordered hover variant="success">
+          <thead>
+            <tr>
+              <th>Kind</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Cash</td>
+              <td>{tot - (parseInt(bank) + parseInt(insurance))}</td>
+            </tr>
+            <tr>
+              <td>Span</td>
+              <td>
+                {" "}
+                <span className="border">{bank}</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Insurance</td>
+              <td>
+                <input
+                  value={insurance}
+                  onChange={(e) => setInsurance(e.target.value)}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Tot</td>
+              <td>
+                {" "}
+                <input value={tot} onChange={(e) => setTot(e.target.value)} />
+              </td>
+            </tr>
+            <tr>
+              <td>Vat</td>
+              <td>
+                <input value={vat} onChange={(e) => setVat(e.target.value)} />
+              </td>
+            </tr>
+            <tr>
+              <td>Tot - Vat</td>
+              <td>
+                <span>{tot - vat} </span>
+              </td>
+            </tr>
+          </tbody>
+          <button onClick={clear} className="bg-danger">
+            clear
+          </button>
+        </Table>
+      </section>
     </div>
   );
 };
