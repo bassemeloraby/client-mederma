8963874ad55249fa285a5c855fb7fce7f4bf55a1
bassem <basem12683@gmail.com>
2023-12-19 07:29:44 +0300

    Are you want to delete =&gt;

--- src/components/CloseModal.js
+++ src/components/CloseModal.js
@@ -14,9 +14,8 @@ const CloseModal = ({ show, handleClose, prodModal, deleteHandler }) => {
         <Modal.Title>Modal heading</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Woohoo, you are reading this text in a modal!
-        {prodModal?.Description}
-        {prodModal?._id}
+        Are you want to delete =&gt;
+        <span className="bg-warning p-2">  {prodModal?.Description}</span>?
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
